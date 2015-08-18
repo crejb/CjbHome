14d47993781d6ec6477b83d73b70aed1a2ad5532
Christopher Bowles <christopherjbowles@gmail.com>
2015-08-18 21:56:54 +1000

    Add blog post entity

--- CjbHome/Controllers/HomeController.cs
+++ CjbHome/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CjbHome.DataAccess;
+using CjbHome.ViewModels.Home;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +10,18 @@ namespace CjbHome.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly BlogPostDb _blogPostDb;
+
+        public HomeController()
+        {
+            _blogPostDb = new BlogPostDb();
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var recentPosts = _blogPostDb.BlogPosts.Where(p => !p.IsDraft).Take(3);
+            var vm = new IndexViewModel { RecentPosts = recentPosts };
+            return View(vm);
         }
 
         public ActionResult About()
---
+++ CjbHome/DataAccess/BlogPostMigrations/Configuration.cs
@@ -0,0 +1,40 @@
+namespace CjbHome.DataAccess.BlogPostMigrations
+{
+    using CjbHome.Models;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<CjbHome.DataAccess.BlogPostDb>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            MigrationsDirectory = @"DataAccess\BlogPostMigrations";
+        }
+
+        protected override void Seed(CjbHome.DataAccess.BlogPostDb context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+
+            context.BlogPosts.AddOrUpdate(
+                p => p.Title,
+                new BlogPost { Title = "First post", IsDraft = false, PostTime = new DateTime(2015, 8, 15), Content = "Here are some words" },
+                new BlogPost { Title = "Second post", IsDraft = false, PostTime = new DateTime(2015, 8, 16), Content = "Here are some more words" },
+                new BlogPost { Title = "Even more post", IsDraft = false, PostTime = new DateTime(2015, 8, 17), Content = "Here are some different words" }
+                );
+        }
+    }
+}
