68351af6d9d25c18179281067459dbd71ddd018e
Christopher Bowles <christopherjbowles@gmail.com>
2015-09-18 21:15:32 +1000

    Enable data export

--- CjbHome/Services/ImportExportService.cs
+++ CjbHome/Services/ImportExportService.cs
@@ -1,5 +1,4 @@
-﻿using CjbHome.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -12,19 +11,46 @@ namespace CjbHome.Services
 {
     public class ImportExportService
     {
-        public static string GetExportedData(IEnumerable<BlogPost> posts)
+        public static string GetExportedData(IEnumerable<CjbHome.Models.BlogPost> posts)
         {
+            var blogPosts = posts.ToList().Select(p => new BlogPost
+            {
+                Id = p.Id,
+                LinkText = p.LinkText,
+                Title = p.Title,
+                PostDate = p.PostDate,
+                PostTime = p.PostTime,
+                Content = p.Content,
+                HeaderImageUrl = p.HeaderImageUrl,
+                Tags = p.Tags.Select(t => t.Title).ToArray()
+            });
+
             XmlSerializer serialiser = new XmlSerializer(typeof(BlogPost[]));
 
             using (StringWriter sw = new StringWriter())
             {
                 using (XmlWriter writer = XmlWriter.Create(sw))
                 {
-                    serialiser.Serialize(writer, posts.ToArray());
+                    serialiser.Serialize(writer, blogPosts.ToArray());
                     var xml = sw.ToString(); // Your XML
                     return xml;
                 }
             }
+        }
+
+        /// <summary>
+        /// Helper class for serialisation
+        /// </summary>
+        public class BlogPost
+        {
+            public int Id { get; set; }
+            public string LinkText { get; set; }
+            public string Title { get; set; }
+            public DateTime PostDate { get; set; }
+            public DateTime PostTime { get; set; }
+            public string Content { get; set; }
+            public string HeaderImageUrl { get; set; }
+            public string[] Tags { get; set; }
         }
     }
 }
